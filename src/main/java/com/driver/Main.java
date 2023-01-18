99e0d61001c7b9b944f8dddf91d152de74d8b192
Mr SUNDAR <m.n.sundar.2022@gmail.com>
2023-01-18 12:12:55 +0530

    solutin 2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,19 +2,19 @@ package com.driver;
 public class Main {
   public static void main(String[] args) {
 
-      B test1 = new B();
+      A test1 = new B();
       test1.meth();
   }
 }
 class A{
-    String meth(){
+    void meth(){
         System.out.println("Invoking method from class A");
-        return "Invoking method from class A";
+//        return "Invoking method from class A";
     }
 }
 class B extends A{
-    String meth(){
+    void meth(){
         System.out.println("Method is overridden in Extendend class B");
-        return "Method is overridden in Extendend class B";
+//        return "Method is overridden in Extendend class B";
     }
 }
